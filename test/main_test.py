77e9f1dbb930eb21c4730b542b43b6cbc102c352
jtovar2 <jtovar2@github.com>
2018-09-05 20:02:22 -0400

    testing globally and shit

--- test/main_test.py
+++ test/main_test.py
@@ -4,15 +4,16 @@ import requests
 import os
 
 PRIVATE_ENCRYPTION_KEY = os.environ['ENCRYPTION_KEY']
-
+PRIVATE_ENCRYPTION_KEY = "a;lkjasdfm,nx.cvhpuioqwerkjbl,absdf"
 
 request_body = {}
 request_body['action'] = 'getbalance'
-request_body['account'] = 'javier'
-request_body['currency'] = 'digibyte'
+request_body['account'] = 'main'
+request_body['currency'] = 'dogecoin'
 
 encoded = jwt.encode(request_body, PRIVATE_ENCRYPTION_KEY, algorithm='HS256')
 
-response = requests.post('http://127.0.0.1:5000/crypto_cli', data=encoded)
+response = requests.post('http://10.0.0.123:5000/crypto_cli', data=encoded)
+#response = requests.post('http://24.98.193.43:5000/crypto_cli', data=encoded)
 print response.text
 
