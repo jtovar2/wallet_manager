7ccab96a4c06d2d06656b15874ec36fa42bb8242
Javier Tovar <javier@Javiers-MacBook-Pro.local>
2023-08-05 12:26:54 -0700

    neww changes

--- wallet_manager.py
+++ wallet_manager.py
@@ -3,11 +3,8 @@ import json
 import jwt
 import subprocess
 import os
-import textract
 
 import datastore_manager
-from google.cloud import storage
-from google.cloud import vision
 
 #for GAE do this shit
 #from jwt.contrib.algorithms.pycrypto import RSAAlgorithm
@@ -24,94 +21,15 @@ with open(public_key_file_path, 'r') as public_key_file:
         WALLET_MANAGER_PUBLIC_KEY = public_key_file.read()
 
 crypto_wallet_route_string = '/crypto_cli'
-text_processor_route_string = '/texextract/<bucket_id>/<blob_id>'
 
 supported_currencies_to_exec_map = {}
 
 
-supported_currencies_to_exec_map['digibyte'] = '/home/javier/digibyte-6.16.2/bin/digibyte-cli'
-
-supported_currencies_to_exec_map['dogecoin'] = '/home/javier/dogecoin-1.10.0/bin/dogecoin-cli'
+supported_currencies_to_exec_map['dogecoin'] = '/home/javilin93/dogecoin-1.14.6/bin/dogecoin-cli'
 
 supported_actions = ['getbalance','getnewaddress', 'move', 'sendfrom', 'gettransaction']
 
 
-##stuff of text extractor
-project = os.environ['PROJECT_ID']
-storage_client = storage.Client()
-vision_client = vision.ImageAnnotatorClient()
-blobs_to_be_processed_kind = 'blobs_processed_{LCP}'.format(LCP=LCP)
-
-blob_processor_dm = datastore_manager.DatastoreManager(project, blobs_to_be_processed_kind, lcp=LCP)
-
-
-
-
-@application.route(text_processor_route_string, methods=['GET'])
-def text_extractor(bucket_id, blob_id):
-    bucket = storage_client.get_bucket(bucket_id)
-
-    blob = storage.blob.Blob(blob_id, bucket)
-
-
-    content = blob.download_as_string()
-    with open(blob_id, 'wb') as file_obj:
-        blob.download_to_file(file_obj)
-
-
-    local_text = None
-    try:
-        text = textract.process(blob_id)
-        print("local**********************")
-        print(text)
-        local_text = text
-        local_text = local_text.replace('\f', '').replace('\n', '')
-
-        print("local*******************")
-    except:
-        print("textract fucked up")
-
-
-    image = vision.types.Image(content=content)
-    gcp_document_text_response = vision_client.document_text_detection(image=image)
-    handWriting_image_context = vision.types.ImageContext(
-        language_hints=['en-t-i0-handwrit'])
-
-
-    gcp_text_response = vision_client.text_detection(image=image)
-    gcp_handwriting_response = vision_client.document_text_detection(image=image,
-                                              image_context=handWriting_image_context)
-
-    os.remove(blob_id)
-    
-    if (gcp_document_text_response.full_text_annotation.text is not None and gcp_document_text_response.full_text_annotation.text != "") or ( local_text is not None and local_text != "") or ( gcp_handwriting_response is not None and gcp_handwriting_response != "") or ( gcp_text_response is not None and gcp_text_response != ""):
-        gcp_document_text = gcp_document_text_response.full_text_annotation.text.encode('ascii', 'ignore')
-        gcp_handwriting_text = gcp_handwriting_response.full_text_annotation.text.encode('ascii', 'ignore')
-        gcp_text = gcp_handwriting_response.full_text_annotation.text.encode('ascii', 'ignore')
-
-        responses = [gcp_handwriting_text, gcp_text, gcp_document_text, local_text]
-
-        for response in responses:
-            if response is None:
-                responses.remove(response)
-
-        responses.sort(key=len, reverse=True)
-
-        json_dict = {}
-        json_dict['local_text'] = local_text
-        json_dict['gcp_document_text'] = gcp_document_text
-        json_dict['gcp_handwriting_text'] = gcp_handwriting_text
-        json_dict['gcp_text'] = gcp_text
-        json_dict['bucket'] = bucket_id
-        json_dict['id'] = str(blob_id)
-        json_dict['official_response'] = responses[0]
-
-        key = blob_processor_dm.add_entity_with_id(json_dict)
-        print(key)
-        return "OK" + str(key)
-
-
-    return abort(403,'unable_to_process_blob')
 
 
 
